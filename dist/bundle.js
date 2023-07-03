4d06a8a78e5bfca2538b19a9f04ede4d5f161fcf
g7g7g8 <artemstanovov36@gmail.com>
2023-07-03 21:07:51 +0300

    Дополнил верстку и стили footer, header как по макету

--- dist/bundle.js
+++ dist/bundle.js
@@ -13,40 +13,58 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   createFooterTemplate: () => (/* binding */ createFooterTemplate)
 /* harmony export */ });
 const createFooterTemplate = () => {
-    return (
-        `<footer class="footer">
-        <div class="footer__top footer-top">
-          <div class="footer-top__left">
-            <a class=""></a>
-            <p class=""></p>
-          </div>
-          <div class="footer-top__bar footer-bar">
-            <ul class="footer-bar__list">
-              <li class="footer-bar__item">Главная</li>
-              <li class="footer-bar__item">Каталог</li>
-              <li class="footer-bar__item">Доставка</li>
-              <li class="footer-bar__item">Контакты</li>
-              <li class="footer-bar__item">Аквариумы</li>
-              <li class="footer-bar__item">Дополнительное оборудование</li>
-            </ul>
-          </div>
-        </div>
-        <div class="footer__bottom">
-          <p class="copyright-memo">© Все права защищены</p>
-          <div class="messengers">
-            <ul class="messengers__list">
-              <li class="messengers__item"></li>
-              <li class="messengers__item"></li>
-              <li class="messengers__item"></li>
-            </ul>
-          </div>
-          <div class="developers-name">
-            <p class="developers-name__desc">Разработано:</p>
-            <p class="developers-name__desc">g7g7g8</p>
-          </div>
-        </div>
-      </footer>`
-    )
+  return (
+    `<footer class="footer">
+    <div class="footer__top footer-top">
+      <div class="footer-top__left">
+        <a href="#" class="shop-logo"></a>
+        <p class="shop-logo-desc">г. Нижний Новгород, ул. Бекетова, д 21</p>
+      </div>
+      <div class="footer-top__bar footer-bar">
+        <ul class="footer-bar__list">
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Главная</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Каталог</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Доставка</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Контакты</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Аквариумы</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Дополнительное оборудование</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <p class="copyright-memo">© Все права защищены</p>
+      <div class="messengers">
+        <ul class="messengers__list">
+          <li class="messengers__item">
+            <a href="#" class="messengers__link messengers__link--telegram-logo"></a>
+          </li>
+          <li class="messengers__item">
+            <a href="#" class="messengers__link messengers__link--facebook-logo"></a>
+          </li>
+          <li class="messengers__item">
+            <a href="#" class="messengers__link messengers__link--vk-logo"></a>
+          </li>
+        </ul>
+      </div>
+      <div class="developers-name">
+        <p class="developers-name__desc">Разработано:</p>
+        <p class="developers-name__desc">g7g7g8</p>
+      </div>
+    </div>
+  </footer>`
+  )
 }
 
 /***/ }),
@@ -68,43 +86,43 @@ const createHeaderTemplate = () => {
           <h2 class="header-top__title">Магазин аквариумов</h2>
           <ul class="header-top__list">
             <li class="header-top__item">
-              <a class="header-top__link" href="#">Закладки</a>
+              <a href="#" class="header-top__link" href="#">Закладки</a>
             </li>
             <li class="header-top__item">
-              <a class="header-top__link" href="#">Корзина</a>
+              <a href="#" id="product-cart" class="header-top__link" href="#">Корзина</a>
             </li>
             <li class="header-top__item">
-              <a class="header-top__link" href="#">Оформить заказ</a>
+              <a href="#" class="header-top__link" href="#">Оформить заказ</a>
             </li>
           </ul>
         </article>
         <article class="header-middle">
           <p class="header-middle__desc">Интернет-магазин аквариумов и смежного оборудования</p>
           <div class="header-middle__contacts">
-            <a class="header-middle__phone">+7 (943) 321-12-12</a>
+            <a href="#" class="header-middle__phone">+7 (943) 321-12-12</a>
             <address class="header-middle__address">г. Нижний Новгород, ул. Бекетова, д 21</address>
           </div>
           <div class="my-cabinet">
             <input class="my-cabinet__input" placeholder="Александр Тюрин">
             <div class="my-cabinet__data">
-              <a class="my-cabinet__orders">Мои заказы</a>
-              <a class="my-cabinet__cabinet">Личный кабинет</a>
+              <a href="#" class="my-cabinet__orders">Мои заказы</a>
+              <a href="#" class="my-cabinet__cabinet">Личный кабинет</a>
             </div>
           </div>
         </article>
         <nav class="navigation">
           <ul class="navigation__list">
             <li class="navigation__item">
-              <a class="navigation__link" href="#">Главная</a>
+              <a href="#" class="navigation__link" href="#">Главная</a>
             </li>
             <li class="navigation__item">
-              <a class="navigation__link" href="#">Каталог</a>
+              <a href="#" class="navigation__link" href="#">Каталог</a>
             </li>
             <li class="navigation__item">
-              <a class="navigation__link" href="#">Доставка</a>
+              <a href="#" class="navigation__link" href="#">Доставка</a>
             </li>
             <li class="navigation__item">
-              <a class="navigation__link" href="#">Контакты</a>
+              <a href="#" class="navigation__link" href="#">Контакты</a>
             </li>
           </ul>
         </nav>
--- src/js/footer.js
+++ src/js/footer.js
@@ -1,36 +1,54 @@
 export const createFooterTemplate = () => {
-    return (
-        `<footer class="footer">
-        <div class="footer__top footer-top">
-          <div class="footer-top__left">
-            <a class=""></a>
-            <p class=""></p>
-          </div>
-          <div class="footer-top__bar footer-bar">
-            <ul class="footer-bar__list">
-              <li class="footer-bar__item">Главная</li>
-              <li class="footer-bar__item">Каталог</li>
-              <li class="footer-bar__item">Доставка</li>
-              <li class="footer-bar__item">Контакты</li>
-              <li class="footer-bar__item">Аквариумы</li>
-              <li class="footer-bar__item">Дополнительное оборудование</li>
-            </ul>
-          </div>
-        </div>
-        <div class="footer__bottom">
-          <p class="copyright-memo">© Все права защищены</p>
-          <div class="messengers">
-            <ul class="messengers__list">
-              <li class="messengers__item"></li>
-              <li class="messengers__item"></li>
-              <li class="messengers__item"></li>
-            </ul>
-          </div>
-          <div class="developers-name">
-            <p class="developers-name__desc">Разработано:</p>
-            <p class="developers-name__desc">g7g7g8</p>
-          </div>
-        </div>
-      </footer>`
-    )
+  return (
+    `<footer class="footer">
+    <div class="footer__top footer-top">
+      <div class="footer-top__left">
+        <a href="#" class="shop-logo"></a>
+        <p class="shop-logo-desc">г. Нижний Новгород, ул. Бекетова, д 21</p>
+      </div>
+      <div class="footer-top__bar footer-bar">
+        <ul class="footer-bar__list">
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Главная</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Каталог</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Доставка</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Контакты</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Аквариумы</a>
+          </li>
+          <li class="footer-bar__item">
+            <a href="#" class="footer-bar__link">Дополнительное оборудование</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <p class="copyright-memo">© Все права защищены</p>
+      <div class="messengers">
+        <ul class="messengers__list">
+          <li class="messengers__item">
+            <a href="#" class="messengers__link messengers__link--telegram-logo"></a>
+          </li>
+          <li class="messengers__item">
+            <a href="#" class="messengers__link messengers__link--facebook-logo"></a>
+          </li>
+          <li class="messengers__item">
+            <a href="#" class="messengers__link messengers__link--vk-logo"></a>
+          </li>
+        </ul>
+      </div>
+      <div class="developers-name">
+        <p class="developers-name__desc">Разработано:</p>
+        <p class="developers-name__desc">g7g7g8</p>
+      </div>
+    </div>
+  </footer>`
+  )
 }
